a1df2691cf6805269f91b8070c40806702f89799
KC Thomas <kc@KCs-MacBook-Air.local>
2025-03-10 19:52:08 -0400

    Use Java 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,18 @@ android {
     buildFeatures {
         compose = true
     }
+
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.3"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
     }
 }
 
